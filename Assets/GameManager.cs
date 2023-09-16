5009c24e99b193609f29c2d6508214c6f4b5f05d
VMarkovicz <johnvicent17@gmail.com>
2023-09-16 14:53:49 -0300

    feat:Red Ghost base algorithm

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pacman;
+
     public GameObject leftWarpNode;
     public GameObject rightWarpNode;
 
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -37,9 +37,11 @@ public class EnemyController : MonoBehaviour
 
     public bool readyToLeaveHome = false;
 
+    public GameManager gameManager;
     // Start is called before the first frame update
     void Awake()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         movementController = GetComponent<MovementController>();
         if(ghostType == GhostType.red)
         {
@@ -74,6 +76,15 @@ public class EnemyController : MonoBehaviour
     public void ReachedCenterOfNode(NodeController nodeController) {
         if(ghostNodeState == GhostNodeStatesEnum.movingInNodes) {
             // Determine next game node to go to
+            if(GhostType.red == ghostType) {
+                DetermineRedGhostDirection();
+            } else if(GhostType.pink == ghostType) {
+                DeterminePinkGhostDirection();
+            } else if(GhostType.blue == ghostType) {
+                DetermineBlueGhostDirection();
+            } else if(GhostType.orange == ghostType) {
+                DetermineOrangeGhostDirection();
+            }
         } else if(ghostNodeState == GhostNodeStatesEnum.respawning){
             // Determine the quickest direction to home
         } else {
@@ -96,4 +107,93 @@ public class EnemyController : MonoBehaviour
             }
         }
     }
+
+
+    void DetermineRedGhostDirection()
+    {
+        string direction = GetClosestDirection(gameManager.pacman.transform.position);
+        movementController.SetDirection(direction);
+    }
+
+
+    void DeterminePinkGhostDirection()
+    {
+        
+    }
+
+
+    void DetermineBlueGhostDirection()
+    {
+        
+    }
+
+    void DetermineOrangeGhostDirection()
+    {
+        
+    }
+
+    string GetClosestDirection(Vector2 target)
+    {
+        float shortestDistance = 0;
+        string lastMovingDirection = movementController.lastMovingDirection;
+        string newDirection = "";
+
+        NodeController nodeController = movementController.currentNode.GetComponent<NodeController>();
+
+        if(nodeController.canMoveUp && lastMovingDirection != "down")
+        {
+            GameObject nodeUp = nodeController.nodeUp;
+
+            float distance = Vector2.Distance(nodeUp.transform.position, target);
+
+            if(distance < shortestDistance || shortestDistance == 0)
+            {
+                shortestDistance = distance;
+                newDirection = "up";
+            }
+        }
+
+        if(nodeController.canMoveDown && lastMovingDirection != "up")
+        {
+            GameObject nodeDown = nodeController.nodeDown;
+
+            float distance = Vector2.Distance(nodeDown.transform.position, target);
+
+            if(distance < shortestDistance || shortestDistance == 0)
+            {
+                shortestDistance = distance;
+                newDirection = "down";
+            }
+        }
+
+        if(nodeController.canMoveLeft && lastMovingDirection != "right")
+        {
+            GameObject nodeLeft = nodeController.nodeLeft;
+
+            float distance = Vector2.Distance(nodeLeft.transform.position, target);
+
+            if(distance < shortestDistance || shortestDistance == 0)
+            {
+                shortestDistance = distance;
+                newDirection = "left";
+            }
+        }
+
+        if(nodeController.canMoveRight && lastMovingDirection != "left")
+        {
+            GameObject nodeRight = nodeController.nodeRight;
+
+            float distance = Vector2.Distance(nodeRight.transform.position, target);
+
+            if(distance < shortestDistance || shortestDistance == 0)
+            {
+                shortestDistance = distance;
+                newDirection = "right";
+            }
+        }
+
+        return newDirection;
+    }
 }
+
+
