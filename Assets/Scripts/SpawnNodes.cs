daba365554a86aa0365d7b2e0027e7c106202c82
VMarkovicz <johnvicent17@gmail.com>
2023-09-18 14:07:47 -0300

    feat/Deletion of Nodes

--- Assets/Scripts/SpawnNodes.cs
+++ Assets/Scripts/SpawnNodes.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class SpawnNodes : MonoBehaviour
 {
     //int numToSpawn = 25;
-    int numToSpawn = 28;
-    public float currentSpawnOffset;
-    public float spawnOffset = 0.299f;
+    // int numToSpawn = 28;
+    // public float currentSpawnOffset;
+    // public float spawnOffset = 0.299f;
     // Start is called before the first frame update
     void Start()
     {
-        if(gameObject.name == "Node")
-        {
-            // gameObject.name = "Node";
-            // return;
-            currentSpawnOffset = spawnOffset;
-            for(int i = 0; i < numToSpawn; i++)
-            {
-                GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x , transform.position.y +currentSpawnOffset, 0), Quaternion.identity);
-                currentSpawnOffset += spawnOffset;
-            }
-        }
+        // if(gameObject.name == "Node")
+        // {
+        //     // gameObject.name = "Node";
+        //     // return;
+        //     currentSpawnOffset = spawnOffset;
+        //     for(int i = 0; i < numToSpawn; i++)
+        //     {
+        //         GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x , transform.position.y +currentSpawnOffset, 0), Quaternion.identity);
+        //         currentSpawnOffset += spawnOffset;
+        //     }
+        // }
     }
 
     // Update is called once per frame
